8d35943d31e7c37960d3195423ae4b1a8e0e46ca
marlon <falconsoft.3d@gmail.com>
2020-09-25 15:24:07 +0200

    sc

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset ="UTF8">
+  <title>Ejemplo Boostrap</title>
+  <meta name="viewport" content="width = device-width">
+  <link href="http://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+  <script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script>
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+      <![endif]-->
+
+
+</head>
+ <body>
+  
+  <div class="container">
+       <h1>Hello</h1>    
+  </div>
+       
+
+ </body>
+</html>
